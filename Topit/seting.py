e0d5545ab1233c8a9b0a58628ee3d6df341f2c25
perterually <wuxinjielovefang@gmail.com>
2016-08-17 14:21:57 +0800

    seting

---
+++ Topit/seting.py
@@ -0,0 +1,15 @@
+Head = {
+    'Connection': 'keep-alive',
+    'Host': 'www.topit.me',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+
+Heads = {
+    'Connection': 'keep-alive',
+    'Cookie': 'is_click=1',
+    'Host': 'www.topit.me',
+    'Referer': 'http://www.topit.me/event/warmup/welcome/views/index.html',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
